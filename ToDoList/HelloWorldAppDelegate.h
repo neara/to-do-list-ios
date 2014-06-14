06b56a9e3664ae3b12836bc2a2c5d8baee674dab
neara <alkadur@gmail.com>
2014-06-14 19:26:43 +0300

    Initial Commit

---
+++ ToDoList/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  ToDoList
+//
+//  Created by Ana Tr on 6/14/14.
+//  Copyright (c) 2014 Ana Tr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Ana Tr on 6/14/14.
+//  Copyright (c) 2014 Ana Tr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HelloWorldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
+    }
+}
